031ef61ee09874abd85155e124324dce559e7d62
ArshFarwaha <arshdeep.farwaha@gmail.com>
2022-10-15 13:27:44 -0400

    React Crash Course

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx"
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div className="todo__wrapper">
+
+      <Todo />
+      <Todo />
+      <Todo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
